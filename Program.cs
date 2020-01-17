207e91cd2ee1d58e5d159a143894ccf7d257bfb4
kristo <kristo@hotmail.co.uk>
2020-01-17 12:54:00 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -38,7 +38,7 @@ namespace SimpleAlgos
         public static string AlgoA1(string input)
         {
 
-            //Write a C# Sharp program to create a new string which is 4 copies of the 2 front characters of a given string.If the given string length is less than 2 return the original string.
+            //Write a C# Sharp function to create a new string which is 4 copies of the 2 front characters of a given string.If the given string length is less than 2 return the original string.
 
             if (input.Trim().Length > 2)
             {
@@ -53,7 +53,7 @@ namespace SimpleAlgos
 
         public static List<int> AlgoA2(List<int> nums)
         {
-            //Write a C# Sharp program to create a new list from a given list of integers removing those values which are less than 4
+            //Write a C# Sharp function to create a new list from a given list of integers removing those values which are less than 4
             List<int> mylist =new List<int>(new int[] { 0, -2, 1, 2, 3, 5, 4, 7, 8 });
            
             return mylist.Where(n => n < 4).ToList();
@@ -62,7 +62,7 @@ namespace SimpleAlgos
 
         public static string AlgoA3(string s1)
         {
-            // Write a C# Sharp program to create a new string from a given string without the first two characters. Keep the first character if it is "p" and keep the second character if it is "y"
+            // Write a C# Sharp function to create a new string from a given string without the first two characters. Keep the first character if it is "p" and keep the second character if it is "y"
             if (s1.Length >= 2)
             {
                 if (String.Compare(s1.Substring(1, 1), "y") != 0)
@@ -86,7 +86,7 @@ namespace SimpleAlgos
 
         public static int[] AlgoA4(int[] numbers)
         {
-            // Write a C# Sharp program to check a given array of integers, length 3 and create a new array. If there is a 5 in the given array immediately followed by a 7 then set 7 to 1
+            // Write a C# Sharp function to check a given array of integers, length 3 and create a new array. If there is a 5 in the given array immediately followed by a 7 then set 7 to 1
             for (var i = 0; i < numbers.Length - 1; i++)
             {
                 if (numbers[i].Equals(5) && numbers[i + 1].Equals(7))
@@ -100,7 +100,7 @@ namespace SimpleAlgos
 
        public static int AlgoA5(int[] nums)
         {
-            // Write a C# Sharp program to check the dif between the largest and smallest values in a list of integers
+            // Write a C# Sharp function to check the dif between the largest and smallest values in a list of integers
             int small_num = 0, biggest_num = 0;
 
             if (nums.Length > 0) small_num = biggest_num = nums[0];
